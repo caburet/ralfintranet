44efb4c9f541daf47c2c4755f53b13ab3bc1ea21
Joel Partida <jpartida@openorange.com>
2017-08-24 22:23:45 -0300

    Agrego archivo nuevo

---
+++ components/layout/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+          <div class="box">
+            <slot></slot>
+            <button  class="button is-primary" @click="$emit('close')">Close</button>
+          </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+//import Tooltip from 'vue-bulma-tooltip'
+//import { mapGetters, mapActions } from 'vuex'
+//import {  LOGIN} from 'vuex-store/mutation-types'
+import store from './../../store'
+export default {
+
+  components: {
+
+  },
+
+  props: {
+
+  },
+
+  computed:{},
+
+  methods: {
+  }
+}
+</script>
+
