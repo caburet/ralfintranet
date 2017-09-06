5589d4aadcbae92390a9df65311f2a4194647b76
Partida Joel <jpartida@openorange.com>
2017-09-06 12:06:35 -0300

    test

--- components/layout/Modal.vue
+++ components/layout/Modal.vue
@@ -38,8 +38,9 @@ export default {
   methods: {
     openInNewTab() {
     console.log(state.app.form)
+    let formid=state.app.form
     store.commit(TOGGLE_MODAL, {'opened':true,'modalcontain':state.app.modalContain, button1:state.app.buttons1, button3:state.app.buttons3,ruleid:state.app.ruleid, form:''} )
-    let url = 'http://develop-ralfprueba.oppen.io/inquiry/viewInquiry?inq=' + state.app.form
+    let url = 'http://develop-ralfprueba.oppen.io/inquiry/viewInquiry?inq=' + formid
       var win = window.open(url, '_blank');
       win.focus();
     }
